6f3baa386bc8025e5336e2b4df7041686abf2bee
Rhein Sullivan <126785035+RheinSullivan@users.noreply.github.com>
2023-05-06 15:10:55 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum 3/Overload1.py
@@ -0,0 +1,5 @@
+# Penggunaan operator +
+
+print(14+41)
+print("keren" + "kece")
+print([1,2] + [3,4])
---
+++ Pertemuan 3/Praktikum 3/Overload2.py
@@ -0,0 +1,8 @@
+# penggunaan metode short
+
+a = [1,4,0,9]
+a.sort()
+print(a)
+b = ["r","i","f","k","i"]
+b.sort()
+print(b)
